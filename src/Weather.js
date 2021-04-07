2d8535ee3035d919c14b9c8954a4d85db2b60db2
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-04-07 15:27:37 -0600

    fixed rendered json data

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react'; 
+import * as React from 'react'; 
 //import axios from 'axios'; 
 import {
   DataGrid
@@ -7,28 +7,10 @@ import './App.css'
 import WeatherInfo from './data/weather.json'
 
 
+const WeatherData = WeatherInfo.results[0].weatherlocation
+
 const Weather = () => {
  
-
-
-
-const [weatherData, setWeatherData] = useState({
-    loading: false,
-    weatherList: [],
-})
-
-
-useEffect(() => {
-    setWeatherData({
-        loading: false, 
-        weatherList: WeatherInfo.results[0].weatherlocation})
-}, [])
-
-
- //const [weatherLocation, setweatherLocation] = useState('')
-
-
-
 
 return (
 
@@ -38,25 +20,28 @@ return (
  
 <div style={{ height: 400, width: '100%' }}>
 <DataGrid
-columns={[ { field: 'City'}, { field: 'Farenheight'}, { field: 'Celcius'} ] }
+columns={[ { field: 'City'}, { field: 'Farenheight'}, { field: 'Celcius'}, { field: 'Daily Forecast'},  { field: 'Hourly Forecast'} ] }
 rows={[
     {
     id: '1',
-    City: `${weatherData.weatherList[0].location}`, 
-    Farenheight: `${weatherData.weatherList[0].farenheight}`,
-    Celcius: `${weatherData.weatherList[0].celcius}`
+    City: `${WeatherData[0].location}`, 
+    Farenheight: `${WeatherData[0].farenheight}`,
+    Celcius: `${WeatherData[0].celcius}`, 
+    Daily_Forecast: `${WeatherData[0].seven_day_forecast[0]}` 
     },
     {
     id: '2',
-    City: `${weatherData.weatherList[1].location}`, 
-    Farenheight: `${weatherData.weatherList[1].farenheight}`,
-    Celcius: `${weatherData.weatherList[1].celcius}`
+    City: `${WeatherData[1].location}`, 
+    Farenheight: `${WeatherData[1].farenheight}`,
+    Celcius: `${WeatherData[1].celcius}`,
+    Daily_Forecast: `${WeatherData[1].seven_day_forecast[0]}` 
     },
     {
-        id: '3',
-        City: `${weatherData.weatherList[2].location}`, 
-        Farenheight: `${weatherData.weatherList[2].farenheight}`,
-        Celcius: `${weatherData.weatherList[2].celcius}`
+    id: '3',
+    City: `${WeatherData[2].location}`, 
+    Farenheight: `${WeatherData[2].farenheight}`,
+    Celcius: `${WeatherData[2].celcius}`,
+    Daily_Forecast: `${WeatherData[2].seven_day_forecast[0]}` 
         }
 ]}
 />
