88f2c1f9c3ac4ec62d6e64bbf8e03b5d5db51630
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-04-06 23:34:25 -0600

    update filter

--- src/Weather.js
+++ src/Weather.js
@@ -1,61 +1,14 @@
 import React, { useEffect, useState} from 'react'; 
 //import axios from 'axios'; 
 import {
-    List, 
-    ListItem, 
-    MenuItem,
-    Menu,
-   // ListItemAvatar, 
-//ListItemIcon, 
-//ListItemSecondaryAction, 
-ListItemText, 
- 
-FormGroup, 
-FormControlLabel, 
-Checkbox, 
-Grid, 
-Typography, 
-FolderIcon, 
-DeleteIcon} from '@material-ui/core'
+  DataGrid
+} from '@material-ui/data-grid'
 import './App.css'
 import WeatherInfo from './data/weather.json'
 
 
 const Weather = () => {
  
-
-function generate(element) {
-    return [0].map((value) => 
-    React.cloneElement(element, {
-        key: value, 
-    }), 
-    )
-}
-
-
-function generate2(element) {
-    return [0].map((value) => 
-    React.cloneElement(element, {
-        key: value, 
-    }), 
-    )
-}
-
-
-function generate3(element) {
-    return [0].map((value) => 
-    React.cloneElement(element, {
-        key: value, 
-    }), 
-    )
-}
-
-
-const [dense, setDense] = React.useState(false)
-const [secondary, setSecondary] = React.useState(false)
-const [thirdOption, setThirdOption] = React.useState(false)
-const [weatherDegreeF, setweatherDegreeF] = React.useState(false)
-const [weatherDegreeC, setweatherDegreeC] = React.useState(false)
 
 
 
@@ -82,89 +35,36 @@ return (
 <div>
 <h1>Weather App</h1>
 
-    <div>
-    <FormGroup row>
-        <FormControlLabel
-        control={
-            <Checkbox checked={dense} onChange={(event) => setDense(event.target.checked)} /> 
+ 
+<div style={{ height: 400, width: '100%' }}>
+<DataGrid
+columns={[ { field: 'City'}, { field: 'Farenheight'}, { field: 'Celcius'} ] }
+rows={[
+    {
+    id: '1',
+    City: `${weatherData.weatherList[0].location}`, 
+    Farenheight: `${weatherData.weatherList[0].farenheight}`,
+    Celcius: `${weatherData.weatherList[0].celcius}`
+    },
+    {
+    id: '2',
+    City: `${weatherData.weatherList[1].location}`, 
+    Farenheight: `${weatherData.weatherList[1].farenheight}`,
+    Celcius: `${weatherData.weatherList[1].celcius}`
+    },
+    {
+        id: '3',
+        City: `${weatherData.weatherList[2].location}`, 
+        Farenheight: `${weatherData.weatherList[2].farenheight}`,
+        Celcius: `${weatherData.weatherList[2].celcius}`
         }
-        label={`${weatherData.weatherList[0].location}`}
-        />
-          <FormControlLabel
-        control={
-            <Checkbox checked={secondary} onChange={(event) => setSecondary(event.target.checked)} /> 
-        }
-        label={`${weatherData.weatherList[1].location}`}
-        />
-          <FormControlLabel
-        control={
-            <Checkbox checked={thirdOption} onChange={(event) => setThirdOption(event.target.checked)} /> 
-        }
-        label={`${weatherData.weatherList[2].location}`}
-        />
-        </FormGroup>
+]}
+/>
 
 
+</div>
 
 
-        <FormGroup row>
-
-            <FormControlLabel
-        control={
-            <Checkbox checked={weatherDegreeF} onChange={(event) => setweatherDegreeF(event.target.checked)} /> 
-        }
-        label="Farenheight"
-        />
-              <FormControlLabel
-        control={
-            <Checkbox checked={weatherDegreeC} onChange={(event) => setweatherDegreeC(event.target.checked)} /> 
-        }
-        label="Celcius" 
-        />
-    </FormGroup>
-
-
-
-    
-    <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-            <Typography variant="h6">{weatherData.location}</Typography>
-            <List dense={dense}>
-                {generate(
-                    <ListItem>
-                        <ListItemText
-                        primary={`${weatherData.farenheight}`}
-                        secondary={secondary ? 'Secondary text' : null}
-                        />
-                    </ListItem>
-                    
-                )}
-                 {generate2(
-                    <ListItem>
-                        <ListItemText
-                        primary={`${weatherData.celcius}`}
-                        secondary={secondary ? 'Secondary text' : null}
-                        />
-                    </ListItem>
-                    
-                )},
-                  {generate3(
-                    <ListItem>
-                        <ListItemText
-                        primary={`${weatherData.seven_day_forecast}`}
-                        secondary={secondary ? 'Secondary text' : null}
-                        />
-                    </ListItem>
-                    
-                )}
-            </List>
-        </Grid>
-
-    </Grid>
-
-
-
-    </div>
 
 
 
