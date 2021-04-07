268b66ed299b0d134489e9fce72a4d0dd8f197ea
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-04-07 16:29:39 -0600

    adjust width per datagrid column

--- src/Weather.js
+++ src/Weather.js
@@ -18,30 +18,30 @@ return (
 <h1>Weather App</h1>
 
  
-<div style={{ height: 400, width: '100%' }}>
+<div style={{ height: 800, width: '100%'}}>
 <DataGrid
-columns={[ { field: 'City'}, { field: 'Farenheight'}, { field: 'Celcius'}, { field: 'Daily Forecast'},  { field: 'Hourly Forecast'} ] }
+columns={[ { field: 'City', width: "200px"}, { field: 'Farenheight', width: '200px'}, { field: 'Celcius', width: '200px'}, { field: 'Daily Forecast', width: '200px'},  { field: 'Hourly Forecast', width: '200px'} ] }
 rows={[
     {
     id: '1',
     City: `${WeatherData[0].location}`, 
     Farenheight: `${WeatherData[0].farenheight}`,
     Celcius: `${WeatherData[0].celcius}`, 
-    Daily_Forecast: `${WeatherData[0].seven_day_forecast[0]}` 
+    Daily_Forecast: `${WeatherData[0].seven_day_forecast}` 
     },
     {
     id: '2',
     City: `${WeatherData[1].location}`, 
     Farenheight: `${WeatherData[1].farenheight}`,
     Celcius: `${WeatherData[1].celcius}`,
-    Daily_Forecast: `${WeatherData[1].seven_day_forecast[0]}` 
+    Daily_Forecast: `${WeatherData[1].seven_day_forecast}` 
     },
     {
     id: '3',
     City: `${WeatherData[2].location}`, 
     Farenheight: `${WeatherData[2].farenheight}`,
     Celcius: `${WeatherData[2].celcius}`,
-    Daily_Forecast: `${WeatherData[2].seven_day_forecast[0]}` 
+    Daily_Forecast: `${WeatherData[2].seven_day_forecast}` 
         }
 ]}
 />
